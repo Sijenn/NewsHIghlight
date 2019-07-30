54fc8448ff8956d66d429c28b7f69c832d06b70c
jenny <jenniffer>
2019-07-30 15:16:56 +0300

    use juquery to facilitate smooth scrolling

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  
     // This facilitates smooth scrolling using jQuery easing
     $('a.smooth-scroll[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
